b733af140dcf62af716196ed8ddf1376f0e25bce
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-08-23 09:22:18 -0300

    add controller

---
+++ backend/src/Controllers/index.ts
@@ -0,0 +1,7 @@
+export { default as AccountController } from './AccountController';
+export { default as AuthController } from './AuthController';
+export { default as LoginController } from './LoginController';
+export * from './Schemas/AccountSchema';
+export * from './Schemas/LoginSchema';
+export * from './Schemas/TransactionSchema';
+export { default as TransactionController } from './TransactionController';
